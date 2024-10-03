9added22511dca520353d9b96624ba1cc71f0b82
Konynunez <konynunez@gmail.com>
2024-10-03 11:48:19 -0400

    note file fixed

---
+++ src/app/note/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotePage = () => {
+  return (
+    <div>
+      <h1>Note Page</h1>
+      <p>This is the content of the note page.</p>
+    </div>
+  );
+};
+
+export default NotePage;
